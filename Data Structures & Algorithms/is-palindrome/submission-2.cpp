41243ce33f08c28150efa1233a587b8d04c959a2
Ajitdada Gharge <ghargeajitdada@gmail.com>
2026-08-10 13:38:47 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0,j=s.size();
+        if(s.size()==1) return true;
+        while(i<=j){
+            while(i<j&&!isalnum(s[i])){
+                i++;
+            }
+            while(i<j&&!isalnum(s[j])){
+                j--;
+            }
+            char a=tolower(s[i]),b=tolower(s[j]);
+            if(a!=b){
+                return false;
+            }
+i++;
+j--;
+            
+        }
+        return true;
+    }
+};
